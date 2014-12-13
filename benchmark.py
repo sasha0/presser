dd568a7543094ace10c249bca1b801b7664512ae
Alexander Gaevsky <sasha@sasha0.ru>
2014-12-13 23:17:08 +0200

    basic implementation of HTTP benchmark

---
+++ benchmark.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from random import shuffle
+from time import time
+from urlparse import urlparse
+import simplejson
+import requests
+from constants import HTTP_RESPONSE_CODES
+
+class Presser:
+    allowed_methods = ['GET', 'POST']
+
+    def __init__(self, url, options):
+        self.url = url
+        self.urls = []
+        self.options = options
+        self.method = getattr(self.options, 'method', 'GET') or 'GET'
+        scenario_path = getattr(options, 'scenario', None)
+        url_list = getattr(self.options, 'url_list', None)
+        shuffle_urls = getattr(self.options, 'random', None)
+
+        # loading list of URLs, credentials and other settings from JSON file
+        if scenario_path:
+            urls = self._load_scenario(scenario_path)
+            for u in urls:
+                u = self._prepare_url(u)
+                r = requests.get(u)
+                # checking if status code is 4xx or 5xx
+                if r.status_code >= 400:
+                    print '%s %s' % (r.status_code, HTTP_RESPONSE_CODES[r.status_code])
+
+        # loading list of URLs from text file
+        if url_list:
+            self._load_url_list(url_list)
+
+        # following URLs from list randomly, not one by one
+        if shuffle_urls and self.urls:
+            shuffle(self.urls)
+
+
+    def validate(self):
+        if not self.url or len(self.urls) == 0:
+            print 'Please provide URL(s) for HTTP benchmarking'
+
+        if self.method not in self.allowed_methods:
+            print 'Request method "%s" is not supported. Please do %s requests.' % (self.method,
+                                                                                    ','.join(self.allowed_methods))
+
+
+    def _prepare_url(self, url):
+        parsed_url = urlparse(url)
+        if not parsed_url.scheme:
+            url = 'http://' + url
+        return url
+
+    def _load_scenario(self, scenario_path):
+        f = open(scenario_path, 'r')
+        content = f.read()
+        content = '"' + content + '"'
+        content = content.replace('\n', '')
+        content = content.replace('\r', '')
+        content = content.replace('\t', '')
+        scenario = simplejson.loads(content)
+        return scenario
+
+    def _load_url_list(self, url_list):
+        f = open(url_list, 'r')
+        content = f.read()
+        urls = content.split('\n')
+        self.urls = filter(None, urls)
+        f.close()
+
+    def start_measure(self):
+        self.start_time = time()
+
+    def stop_measure(self):
+        return time() - self.start_time
+
+    def run_benchmark(self):
+        self.validate()
+        if self.method == 'GET':
+            urls = self.urls or [self.url]
+            for url in urls:
+                url = self._prepare_url(url)
+                self.start_measure()
+                r = requests.get(url)
+                # checking if status code is 4xx or 5xx
+                if r.status_code >= 400:
+                    print '%s %s' % (r.status_code, HTTP_RESPONSE_CODES[r.status_code])
+                spent_time = self.stop_measure()
+                print '%s - %.2fs' % (url, spent_time)
---
+++ presser.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from optparse import OptionParser
+from benchmark import Presser
+                   
+parser = OptionParser()
+parser.add_option('-m', '--method', dest='method', action='store', type='string', help='POST or GET HTTP method')
+parser.add_option('-r', '--rate', dest='rate', action='store', type="int", help='Number or requests per second')
+parser.add_option('-c', '--requests', dest='requests', action='store', help='Number of concurrent requests')
+parser.add_option('-q', '--random', dest='random', action='store_true', help='Do requests to list of URLs randomly.')
+parser.add_option('-l', '--list', dest='url_list', action='store', type='string', help='List of URLs for benchmarking.')
+parser.add_option('-s', '--scenario', dest='scenario', action='store', type='string', help='Path to file with benchmarking scenario')
+parser.add_option('-f', '--follow', dest='follow_redirection', action='store_true', help='Follow or skip redirection')
+parser.add_option('-n', '--number', dest='requests', action='store', type='int', help='Total number of requests')
+parser.add_option('-a', '--auth', dest='auth', action='store', help='HTTP authorization credentials')
+
+
+(options, args) = parser.parse_args()
+
+if __name__ == '__main__':
+    url = args[0] if len(args) > 0 else None
+    presser = Presser(url, options)
+    presser.run_benchmark()
